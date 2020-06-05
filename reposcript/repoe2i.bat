0f0796b9e2c1217d73d9489fe65952173b7c704d
sammtcbn <sammtcbn@gmail.com>
2020-06-05 15:02:39 +0800

    script for clone e2i project

---
+++ reposcript/repoe2i.bat
@@ -0,0 +1,22 @@
+@echo off
+
+git clone https://gitlab.edgecenter.io/edgesense/mqttbroker.git
+
+git clone https://gitlab.edgecenter.io/edgesense/support-i-alarm-node.js.git
+
+git clone https://gitlab.edgecenter.io/edgesense/device-net-monitor.git
+
+git clone https://gitlab.edgecenter.io/edgesense/device-process-monitor.git
+
+git clone https://gitlab.edgecenter.io/edgesense/device-opcua-client-node.js.git
+
+git clone https://gitlab.edgecenter.io/edgesense/industrial-protocol-sdk.git
+git clone https://gitlab.edgecenter.io/edgesense/device-industry-node.js.git
+
+git clone -b sensor-flows https://gitlab.edgecenter.io/eiss_cloud_solution_team/node-red.git
+
+git clone https://gitlab.edgecenter.io/edgesense/e2i-edge-webconsole.git
+
+git clone https://gitlab.edgecenter.io/edgesense/e2i-edge-installer.git
+
+git clone https://gitlab.edgecenter.io/edgesense/e2i-server-installer.git
