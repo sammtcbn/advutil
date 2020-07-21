507f5c55248cb37645601b749157f060b0097038
sammtcbn <sammtcbn@gmail.com>
2020-07-21 18:12:06 +0800

    update repo url

--- reposcript/repoe2i.bat
+++ reposcript/repoe2i.bat
@@ -13,6 +13,8 @@ git clone https://gitlab.edgecenter.io/edgesense/device-opcua-client-node.js.git
 git clone https://gitlab.edgecenter.io/edgesense/industrial-protocol-sdk.git
 git clone https://gitlab.edgecenter.io/edgesense/device-industry-node.js.git
 
+git clone https://gitlab.edgecenter.io/edgesense/device-wascada-handler.git
+
 git clone -b sensor-flows https://gitlab.edgecenter.io/eiss_cloud_solution_team/node-red.git
 
 git clone https://gitlab.edgecenter.io/edgesense/e2i-edge-webconsole.git
@@ -24,3 +26,5 @@ git clone https://gitlab.edgecenter.io/edgesense/e2i-server-installer.git
 git clone https://gitlab.edgecenter.io/edgesense/e2i-container-maker.git
 
 git clone https://gitlab.edgecenter.io/edgesense/grafana.git
+
+git clone https://gitlab.edgecenter.io/edgesense/DBMService.git
